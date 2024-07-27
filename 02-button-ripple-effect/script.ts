885d3dfd157712f1ae25cd4492d3e4c532d222f8
denyschr <denprogman99@gmail.com>
2024-07-27 17:56:46 +0300

    feat: create button ripple effect

---
+++ 02-button-ripple-effect/script.ts
@@ -0,0 +1,27 @@
+function replicateRipple(event: MouseEvent): void {
+  const target = event.currentTarget as HTMLElement;
+  const circle = document.createElement('span');
+  const diameter = Math.max(target.clientWidth, target.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - (target.offsetLeft + radius)}px`;
+  circle.style.top = `${event.clientY - (target.offsetTop + radius)}px`;
+  circle.classList.add('_ripple');
+
+  const ripple = target.querySelectorAll<HTMLElement>('._ripple')[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  target.appendChild(circle);
+}
+
+const rippleButtons = document.querySelectorAll<HTMLElement>('.btn-ripple');
+
+if (rippleButtons.length) {
+  rippleButtons.forEach(button => {
+    button.addEventListener('mousedown', replicateRipple);
+  });
+}
